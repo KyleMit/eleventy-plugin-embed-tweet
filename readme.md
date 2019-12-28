5d3c19fd036501d62d320398494f46ede09cf412
Kyle Mitofsky <kylemit@gmail.com>
2019-12-28 18:45:56 -0500

    refactor twitter module, styles, and update readme

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 
-## Install
+## Installation
 
 Install [on npm](https://www.npmjs.com/package/eleventy-plugin-embed-tweet)
 
@@ -7,13 +7,28 @@ Install [on npm](https://www.npmjs.com/package/eleventy-plugin-embed-tweet)
 npm install eleventy-plugin-embed-tweet --save
 ```
 
-## Note
+Add the following to your .`eleventy.js` config - see docs for options param:
 
-Requires signing up for free [twitter developer account](https://developer.twitter.com/en/apply-for-access)
+```js
+module.exports = function(eleventyConfig) {
 
-## Usage
+    const pluginEmbedTweet = require("eleventy-plugin-embed-tweet")
+    eleventyConfig.addPlugin(pluginEmbedTweet);
+
+};
+```
+
+## Requirements
+
+Requires signing up for free [twitter developer API account](https://developer.twitter.com/en/apply-for-access) - see docs for walkthrough
+
+This plugin relies on making live API calls during build by using async shortcodes which were added in [**Eleventy v0.10.0**](https://github.com/11ty/eleventy/releases/tag/v0.10.0-beta.1) so it's listed as a peer dependency
+
+## Basic Usage
 
 Embed a tweet anywhere you can use nunjucks templates using a shortcode with the tweet id like this:
+
+### Nunjucks
 
 ```js
 {% tweet "1188837207206977536" %}
@@ -21,7 +36,7 @@ Embed a tweet anywhere you can use nunjucks templates using a shortcode with the
 
 > **Note**: ID must be passed as a string because [long numbers will truncate in JS](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER))
 
-### Demo
+## Demo
 
 Demo project available on github at [KyleMit/eleventy-plugin-embed-tweet-demo](https://github.com/KyleMit/eleventy-plugin-embed-tweet-demo) and live version hosted on [eleventy-embed-tweet.netlify.com/](https://eleventy-embed-tweet.netlify.com/)
 
@@ -33,8 +48,20 @@ Demo project available on github at [KyleMit/eleventy-plugin-embed-tweet-demo](h
 * Leverage free (personal) twitter API tier on build server
 * Provide fallback for local development for distributed devs who want to run without adding credentials
 
+## Todo
 
-## Setting .ENV variables
+* [ ] Destructure API JSON before caching - only store info we care about
+* [ ] Cache profile and media images
+* [ ] Much better docs
+* [ ] Figure out more consistent CSS structure
+* [ ] Handle `<br>` elements
+* [ ] Automate `npm publish`
+
+## Docs
+
+### Setting up Twitter Account
+
+### Setting .ENV variables
 
 Once you sign up for a twitter account, you'll need to create a file named `.env` at the project root - it contains keys so it's excluded by the `.gitignore` so each person will have to manually create their own:
 
@@ -45,13 +72,87 @@ CONSUMER_KEY=********
 CONSUMER_SECRET=********
 ```
 
-You'll also have to add the environment variables on your build server
+Remember to update your `.gitignore` with `.env` so you don't commit your secrets
 
-## Todo
+You'll also have to add the environment variables on your build server.
 
-* [ ] Destructure API JSON before caching - only store info we care about
-* [ ] Cache profile and media images
-* [ ] Much better docs
-* [ ] Figure out more consistent CSS structure
-* [ ] Handle `<br>` elements
-* [ ] Automate `npm publish`
+If you build without setting environment variables, the API call won't work, so the plugin will fallback to using the client side embed available from [publish.twitter.com](https://publish.twitter.com/#)
+
+### Plugin Options
+
+```js
+let pluginEmbedTweet = require("eleventy-plugin-embed-tweet")
+
+let tweetEmbedOptions = {
+    cacheDirectory: '',    // default: ''
+    useInlineStyles: true  // default: true
+}
+
+eleventyConfig.addPlugin(pluginEmbedTweet, tweetEmbedOptions);
+```
+
+#### `useInlineStyles`
+
+By default the single embed will contain all the necessary html and css to render each tweet.  This occurs by including a `<style>` block along with the tweet.  If you're embedding multiple tweets per page, you may elect to separate the CSS into it's own area so it's not repeated multiple times
+
+You can instantiate the plugin like this:
+
+```js
+eleventyConfig.addPlugin(pluginEmbedTweet, {useInlineStyles: false});
+```
+
+And then make sure to include the styles somewhere else on your page like this:
+
+```html
+<style type="text/css">{% tweetStyles %}</style>
+```
+
+**Feel free to also add your own styles** and customize however you'd like.  The official twitter widget is implemented as a web component and [makes styling through the shadow dom really tricky](https://stackoverflow.com/a/59493027/1366033) - but this sits as flat html on your page - so customize however you'd like
+
+The rough structure of the html returned by this plugin looks like this:
+
+```css
+.tweet-card
+    .tweet-header
+        .tweet-profile
+        .tweet-author
+            .tweet-author-name
+            .tweet-author-handle
+        .tweet-bird
+            .tweet-bird-icon
+    .tweet-body
+    .tweet-images
+    .tweet-footer
+        .tweet-like
+            .tweet-like-icon.tweet-icon
+            .tweet-like-count
+        .tweet-date
+```
+
+#### `cacheDirectory`
+
+Relying on an external, authenticated API to build your site has some tradeoffs:
+
+* Build will fail if API is down (fire at twitter)
+* Build will fail if you're offline (on a train)
+* People who clone the repo will need to sign up for their own API keys in order to do a full build
+* Increases build times with multiple repetitive calls (especially during debugging)
+* Increases reliance on API over long term
+
+To address these tradeoffs, this plugin can cache API calls locally which you can periodically commit to your repository.
+
+To enable, you need to pass a cacheDirctory path relative to your project root where you'd like to have data saved:
+
+```js
+eleventyConfig.addPlugin(pluginEmbedTweet, {cacheDirectory: './tweets'});
+```
+
+Because this directory will be updated during build time, you must add a [`.eleventyignore`](https://www.11ty.dev/docs/ignores/) with the directories name, otherwise `eleventy --serve` can get stuck in an infinite loop when it detects changes and tries to rebuild the site during the build.
+
+**File**: `.eleventyignore`
+
+```bash
+tweets/
+```
+
+I'd recommend periodically committing this file - the data should be largely unchanged as long as you've captured a new tweet, but the `favorites_count` may want to get refreshed over time.
--- twitter.js
+++ twitter.js
@@ -2,10 +2,294 @@ require('dotenv').config()
 const request = require('request-promise')
 const { promises: fs } = require("fs");
 
+
 module.exports = {
     getTweet,
     getStyles
 }
+
+async function getTweet(tweetId, options) {
+    
+    // if we using cache and not cache busting, check there first
+    if (options.cacheDirectory && !process.env.CACHE_BUST) {
+        let cachedTweets = await getCachedTweets(options);
+        let cachedTweet = cachedTweets[tweetId]
+       
+        // if we have a cached tweet, use that
+        if (cachedTweet ) {
+            return formatTweet(cachedTweet, options)
+        }
+        // else continue on
+    }
+
+
+    // if we have env variables, go get tweet
+    if (hasAuth()) {
+        let liveTweet = await fetchTweet(tweetId)
+
+        let tweetViewModel = processTweet(liveTweet)
+
+        tweetViewModel.html = renderTweet(tweetViewModel)
+
+        // cache tweet
+        if (options.cacheDirectory) {
+            await addTweetToCache(tweetViewModel, options)
+        }
+
+        // build
+        return formatTweet(tweetViewModel, options)
+    } else {
+        console.warn("Remeber to add your twitter credentials as environement variables")
+        console.warn("Read More at https://github.com/KyleMit/eleventy-plugin-embed-tweet#setting-env-variables")
+        // else continue on
+    }
+
+    // finally fallback to client-side injection
+    var htmlTweet =
+        `<blockquote class="twitter-tweet"><a href="https://twitter.com/user/status/${tweetId}"></a></blockquote>` + 
+        `<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
+
+    return htmlTweet
+}
+
+/* Twitter API Call */
+function hasAuth() {
+    return process.env.TOKEN && 
+           process.env.TOKEN_SECRET && 
+           process.env.CONSUMER_KEY && 
+           process.env.CONSUMER_SECRET
+}
+
+function getAuth() {
+    let oAuth = {
+        token: process.env.TOKEN,
+        token_secret: process.env.TOKEN_SECRET,
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+    }
+    return oAuth;
+}
+
+async function fetchTweet(tweetId) {
+    // fetch tweet
+    let apiURI = `https://api.twitter.com/1.1/statuses/show/${tweetId}.json?tweet_mode=extended`
+    let auth = getAuth()
+
+    try {
+        let response = await request.get(apiURI, { oauth: auth });
+        let tweet = JSON.parse(response)
+
+        return tweet
+
+    } catch (error) {
+        // unhappy path - continue to other fallbacks
+        console.log(error)
+        return {}
+    }
+}
+
+/* transform tweets */
+function processTweet(tweet) {
+
+    // parse complicated stuff
+    let images = getTweetImages(tweet)
+    let created_at = getTweetDates(tweet)
+    let htmlText = getTweetTextHtml(tweet)
+    
+    // destructure only properties we care about
+    let {id_str, favorite_count } = tweet
+    let {name, screen_name, profile_image_url_https } = tweet.user
+    let user = {name, screen_name, profile_image_url_https }
+
+    // build tweet with properties we want
+    let tweetViewModel = {
+        id_str, 
+        htmlText,
+        images,
+        favorite_count,
+        created_at,
+        user
+    }
+
+    return tweetViewModel
+}
+
+function getTweetImages(tweet) {
+    let images = []
+    for (media of tweet.entities.media || []) {
+        images.push(media.media_url_https)
+    }
+    return images
+}
+
+function getTweetDates(tweet) {
+    let moment = require("moment");
+
+    // parse
+    let dateMoment = moment(tweet.created_at, "ddd MMM D hh:mm:ss Z YYYY");
+
+    // format
+    let display = dateMoment.format("hh:mm A · MMM D, YYYY")
+    let meta = dateMoment.utc().format("MMM D, YYYY hh:mm:ss (z)")
+
+    return { display, meta }
+}
+
+function getTweetTextHtml(tweet) {
+    let replacements = []
+
+    // hashtags
+    for (hashtag of tweet.entities.hashtags || []) {
+        let oldText = getOldText(tweet.full_text, hashtag.indices)
+        let newText = `<a href="https://twitter.com/hashtag/${oldText.substr(1)}">${oldText}</a>`
+        replacements.push({ oldText, newText })
+    }
+
+    // users
+    for (user of tweet.entities.user_mentions || []) {
+        let oldText = getOldText(tweet.full_text, user.indices)
+        let newText = `<a href="https://twitter.com/${oldText.substr(1)}">${oldText}</a>`
+        replacements.push({ oldText, newText })
+    }
+
+    // urls
+    for (url of tweet.entities.urls || []) {
+        let oldText = getOldText(tweet.full_text, url.indices)
+        let newText = `<a href="${url.expanded_url}">${url.expanded_url.replace(/https?:\/\//,"")}</a>`
+        replacements.push({ oldText, newText })
+    }
+
+    // media
+    for (media of tweet.entities.media || []) {
+        let oldText = getOldText(tweet.full_text, media.indices)
+        let newText = `` // get rid of img url in tweet text
+        replacements.push({ oldText, newText })
+    }
+
+    // make updates at the end
+    let htmlText = tweet.full_text
+    for (rep of replacements) {
+        htmlText = htmlText.replace(rep.oldText, rep.newText)
+    }
+
+    // preserve line breaks to survive minification
+    htmlText = htmlText.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+
+    return htmlText
+}
+
+function getOldText(text, indices) {
+    let startPos = indices[0];
+    let endPos = indices[1];
+    let len = endPos - startPos
+
+    let oldText = text.substr(startPos, len)
+
+    return oldText
+}
+
+
+/* render tweets */
+function renderTweet(tweet) {
+    // get module directory
+    let path = require("path")
+    let moduleDir = path.parse(__filename).dir
+
+    // configure nunjucks
+    let nunjucks = require("nunjucks")
+    nunjucks.configure(moduleDir, { autoescape: true });
+    
+    // render with nunjucks
+    let htmlTweet = nunjucks.render("tweet.njk", tweet);
+
+    // minify before returning
+    // important when injected into markdown to prevent injection of `<p>` tags due to whitespace
+    let htmlMin = minifyHtml(htmlTweet)
+
+    return htmlMin
+}
+
+async function formatTweet(tweet, options) {
+
+    // add css if requested
+    if (options.useInlineStyles) {
+        let styles = await getStyles()
+        let stylesHtml = `<style type='text/css'>${styles}</style>`
+        let stylesMin = minifyHtml(stylesHtml)
+
+        return stylesMin + tweet.html
+    }
+
+    return tweet.html
+}
+
+function minifyHtml(htmlSource) {
+    var minify = require('html-minifier').minify;
+    var result = minify(htmlSource, {
+        minifyCSS: true,
+        collapseWhitespace: true
+    });
+
+    return result;
+}
+
+
+/* caching / file access */
+async function getCachedTweets(options) {
+    let cachePath = getCachedTweetPath(options)
+
+    try {
+        let file = await fs.readFile(cachePath, "utf8")
+        cachedTweets = JSON.parse(file) || {}
+
+        return cachedTweets
+
+    } catch (error) {
+        // otherwise, empty array is fine
+        console.log(error)
+        return {}
+    }
+}
+
+async function addTweetToCache(tweet, options) {
+    try {
+        // get cache
+        let cachedTweets = await getCachedTweets(options)
+
+        // add new tweet
+        cachedTweets[tweet.id_str] = tweet
+
+        // build new cache string
+        let tweetsJSON = JSON.stringify(cachedTweets, 2, 2)
+        
+        let cachePath = getCachedTweetPath(options)
+        let cacheDir = require("path").dirname(cachePath)
+
+        // makre sure directory exists
+        await fs.mkdir(cacheDir, {recursive: true})
+
+        await fs.writeFile(cachePath, tweetsJSON)
+
+        console.log(`Writing ${cachePath}`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function getCachedTweetPath(options) {
+    let path = require("path")
+
+    // get directory for main thread
+    let appPath = require.main.filename       // C:\user\github\app\node_modules\@11ty\eleventy\cmd.js
+    let pos = appPath.indexOf("node_modules")
+    let appRoot = appPath.substr(0, pos)      // C:\user\github\app\
+
+    // build cache file path
+    let cachePath = path.join(appRoot, options.cacheDirectory, "tweets.json")
+
+    return cachePath
+}
+
 
 async function getStyles() {
     // get module directory
@@ -18,197 +302,3 @@ async function getStyles() {
     return styles
 }
 
-
-function minHtml(htmlSource) {
-    var minify = require('html-minifier').minify;
-    var result = minify(htmlSource, {
-        minifyCSS: true,
-        collapseWhitespace: true
-    });
-
-    return result;
-}
-
-async function getTweet(tweetId, options) {
-
-    let cachedTweets = []
-    if (options.cacheDirectory) {
-        try {
-            let file = await fs.readFile("./cache/tweets.json")
-            cachedTweets = JSON.parse(file) || []
-        } catch (error) {
-            // otherwise, empty array is fine
-            console.log(error)
-        }
-    }
-
-
-    let cachedTweet = cachedTweets.find(t => t.id_str === tweetId)
-
-    // if we have a cached tweet, use that
-    if (cachedTweet && !process.env.CACHE_BUST) {
-        return buildTweet(cachedTweet, options)
-    }
-
-    // TODO - reminder to add .env file with instructions
-    // if we have env variables, go get tweet
-    if (process.env.TOKEN && process.env.TOKEN_SECRET && process.env.CONSUMER_KEY && process.env.CONSUMER_SECRET) {
-        // fetch tweet
-        let apiURI = `https://api.twitter.com/1.1/statuses/show/${tweetId}.json?tweet_mode=extended`
-        let oAuth = {
-            token: process.env.TOKEN,
-            token_secret: process.env.TOKEN_SECRET,
-            consumer_key: process.env.CONSUMER_KEY,
-            consumer_secret: process.env.CONSUMER_SECRET,
-        }
-        try {
-            let resp = await request.get(apiURI, { oauth: oAuth });
-            let liveTweet = JSON.parse(resp)
-
-            // cache tweet
-            if (options.cacheDirectory) {
-                try {
-                    cachedTweets.push(liveTweet)
-                    let tweetsJSON = JSON.stringify(cachedTweets, 2, 2)
-                    await fs.writeFile("./cache/tweets.json", tweetsJSON)
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-
-            // build
-            return buildTweet(liveTweet, options)
-
-        } catch (error) {
-            // unhappy path - continue to other fallbacks
-            console.log(error)
-        }
-
-
-    }
-
-    // finally fallback to client-side injection
-    var htmlTweet =
-        `<blockquote class="twitter-tweet">
-        <a href="https://twitter.com/anon/status/${tweetId}"></a>
-    </blockquote>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
-
-    return htmlTweet
-}
-
-async function buildTweet(tweet, options) {
-
-    let { body, images } = getTweetContents(tweet)
-
-    let { dateDisplay, dateMeta } = getTweetDates(tweet)
-
-    // assign extra properties to tweet
-    tweet.body = body
-    tweet.images = images
-    tweet.dateDisplay = dateDisplay
-    tweet.dateMeta = dateMeta
-
-
-    // get module directory
-    let path = require("path")
-    let moduleDir = path.parse(__filename).dir
-    let templatePath = path.join(moduleDir, "/tweet.njk")
-
-    // render with nunjucks
-    let nunjucks = require("nunjucks")
-    nunjucks.configure(moduleDir, { autoescape: true });
-    htmlTweet = nunjucks.render("tweet.njk", tweet);
-
-
-    // add css if requested
-    if (options.useInlineStyles) {
-        let styles = await getStyles()
-        htmlTweet = `<style type='text/css'>${styles}</style>` + htmlTweet
-    }
-
-    // minify before returning
-    let htmlMin = minHtml(htmlTweet)
-
-
-    return htmlMin
-}
-
-function getTweetContents(tweet) {
-    let htmlText = tweet.full_text
-
-    let replacements = []
-    let images = []
-
-    // hashtags
-    for (hashtag of tweet.entities.hashtags || []) {
-        let { startPos, endPos, len } = getIndexPos(hashtag.indices)
-
-        let oldText = htmlText.substr(startPos, len)
-        let newText = `<a href="https://twitter.com/${oldText}">${oldText}</a>`
-
-        replacements.push({ oldText, newText })
-    }
-
-    // users
-    for (user of tweet.entities.user_mentions || []) {
-        let { startPos, endPos, len } = getIndexPos(user.indices)
-
-        let oldText = htmlText.substr(startPos, len)
-        let newText = `<a href="https://twitter.com/hashtag/${user.screen_name}">${oldText}</a>`
-
-        replacements.push({ oldText, newText })
-    }
-
-    // urls
-    for (url of tweet.entities.urls || []) {
-        let { startPos, endPos, len } = getIndexPos(url.indices)
-
-        let oldText = htmlText.substr(startPos, len)
-        let newText = `<a href="${url.expanded_url}">${url.expanded_url.replace(/https?:\/\//,"")}</a>`
-
-        replacements.push({ oldText, newText })
-    }
-
-    // media
-    for (media of tweet.entities.media || []) {
-        let { startPos, endPos, len } = getIndexPos(media.indices)
-
-        let oldText = htmlText.substr(startPos, len)
-        let newText = `` // get rid of img url in tweet text
-
-        replacements.push({ oldText, newText })
-        images.push(media.media_url_https)
-    }
-
-    // make updates at the end
-    for (rep of replacements) {
-        htmlText = htmlText.replace(rep.oldText, rep.newText)
-    }
-
-    return { body: htmlText, images }
-}
-
-function getIndexPos(indices) {
-
-    let startPos = indices[0];
-    let endPos = indices[1];
-    let len = endPos - startPos
-
-    return {
-        startPos,
-        endPos,
-        len
-    }
-}
-
-function getTweetDates(tweet) {
-    let moment = require("moment");
-
-    let dateMoment = moment(tweet.created_at, "ddd MMM D hh:mm:ss Z YYYY");
-
-    let dateDisplay = dateMoment.format("hh:mm A · MMM D, YYYY")
-    let dateMeta = dateMoment.utc().format("MMM D, YYYY hh:mm:ss (z)")
-
-    return { dateDisplay, dateMeta }
-}
