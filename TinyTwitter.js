3c17012ff3f556ab915cbd11ac79698b0d0a4f38
Kyle Mitofsky <KyleMit@gmail.com>
2019-12-28 10:56:15 -0500

    convert tweet template to nunjucks

--- TinyTwitter.js
+++ TinyTwitter.js
@@ -14,7 +14,7 @@ async function getStyles() {
     let stylePath = path.join(moduleDir, "/tweet.css")
 
     let styles = await fs.readFile(stylePath, "utf8")
-    
+
     return styles
 }
 
@@ -41,7 +41,7 @@ async function getTweet(tweetId, options) {
             console.log(error)
         }
     }
-    
+
 
     let cachedTweet = cachedTweets.find(t => t.id_str === tweetId)
 
@@ -50,6 +50,7 @@ async function getTweet(tweetId, options) {
         return buildTweet(cachedTweet, options)
     }
 
+    // TODO - reminder to add .env file with instructions
     // if we have env variables, go get tweet
     if (process.env.TOKEN && process.env.TOKEN_SECRET && process.env.CONSUMER_KEY && process.env.CONSUMER_SECRET) {
         // fetch tweet
@@ -74,7 +75,7 @@ async function getTweet(tweetId, options) {
                     console.log(error)
                 }
             }
-           
+
             // build
             return buildTweet(liveTweet, options)
 
@@ -102,38 +103,28 @@ async function buildTweet(tweet, options) {
 
     let { dateDisplay, dateMeta } = getTweetDates(tweet)
 
-    let htmlTweet =
-        `<blockquote class="static-tweet">
-  <div class="tweet-header">
-    <a class="tweet-profile" href="https://twitter.com/${tweet.user.screen_name}" >
-      <img src="${tweet.user.profile_image_url_https}" alt="Avatar for ${tweet.user.screen_name}" />
-    </a>
-    <div class="tweet-author">
-      <a class="tweet-author-name" href="https://twitter.com/${tweet.user.screen_name}" >${tweet.user.name}</a>
-      <a class="tweet-author-handle" href="https://twitter.com/${tweet.user.screen_name}" >@${tweet.user.screen_name}</a>
-    </div>
-    <a class="tweet-bird" href="https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}">
-      <div class="tweet-bird-icon" aria-label="View on Twitter" title="View on Twitter" role="presentation"></div>
-    </a>
-  </div>
-  <p class="tweet-body">${body}</p>
-  <div class="tweet-images">${images.map(url => `<img alt='Image from Tweet' src='${url}' />`).join('')}</div>
-  <div class="tweet-footer">
-    <a class="tweet-heart" href="https://twitter.com/intent/like?tweet_id=${tweet.id_str}">
-      <div class="tweet-heart-icon" aria-label="Like" title="Like" role="img"></div>
-      <span class="tweet-favorite-count">${tweet.favorite_count}</span>
-    </a>
-    <a class="tweet-date" href="https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}" title="Time Posted: ${dateMeta}">
-      ${dateDisplay}
-    </a>
-  </div>
-</blockquote>`
+    // assign extra properties to tweet
+    tweet.body = body
+    tweet.images = images
+    tweet.dateDisplay = dateDisplay
+    tweet.dateMeta = dateMeta
+
+
+    // get module directory
+    let path = require("path")
+    let moduleDir = path.parse(__filename).dir
+    let templatePath = path.join(moduleDir, "/tweet.njk")
+
+    // render with nunjucks
+    let nunjucks = require("nunjucks")
+    nunjucks.configure(moduleDir, { autoescape: true });
+    htmlTweet = nunjucks.render("tweet.njk", tweet);
 
 
     // add css if requested
     if (options.useInlineStyles) {
         let styles = await getStyles()
-        htmlTweet =  `<style type='text/css'>${styles}</style>` + htmlTweet
+        htmlTweet = `<style type='text/css'>${styles}</style>` + htmlTweet
     }
 
     // minify before returning
